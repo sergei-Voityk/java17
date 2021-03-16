39b048f72b1e03b31eaa6f819060a3a6043cb65c
sergei-Voityk <80097161+sergei-Voityk@users.noreply.github.com>
2021-03-16 21:30:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var myNudelist = document.getElementsByTagName('LI');
+var i;
+for (i=0; i <myNudelist.length; i++){
+    var span = document.createTextNode("u00D7");
+    var txt = document.createTextNode("\u00D7")
+    span.className = "close";
+    span.appendChild(txt);
+    myNudelist[i].appendChild(span)
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i=0; i <close.length; i++) {
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+    if(ev.target.tagname === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+},false);
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createElement(inputValue);
+    li.appendChild(t);
+    if (inputValue === ''){
+        alert('Поле не может быть пустым!')
+    }else {
+        document.getElementById("myUL").appendChild(li);
+    }
+
+}
+
+document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+      close[i].oneclick = function () {
+          var div = this.parentElement;
+          div.style.display = "none"
+      }
+  }
